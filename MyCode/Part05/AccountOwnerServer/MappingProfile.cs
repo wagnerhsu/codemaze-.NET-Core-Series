73e2f484c5dbbcaf4372205e44b0570f9cf258cc
Wagner Xu <wagnerhsu@hotmail.com>
2022-01-25 21:00:50 +0800

    add files

---
+++ MyCode/Part05/AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace AccountOwnerServer
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Owner, OwnerDto>();
+
+            CreateMap<Account, AccountDto>();
+        }
+    }
+}
